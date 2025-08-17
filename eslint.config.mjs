744c5a9ed98fb5516df16881752303c28b0d6be0
HarshDikshit <93562022+HarshDikshit@users.noreply.github.com>
2025-08-17 12:23:47 +0530

    Disable linting rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,12 +10,14 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-  "rules": {
-    "@typescript-eslint/no-explicit-any": "off"
-  }
-}
+  ...compat.config({
+    extends: ["next/core-web-vitals", "next/typescript"],
+    rules: {
+      "react/no-unescaped-entities": "off",
+      "@next/next/no-page-custom-font": "off",
+      "@typescript-eslint/no-explicit-any": "off"
+    },
+  }),
 ];
 
 export default eslintConfig;
